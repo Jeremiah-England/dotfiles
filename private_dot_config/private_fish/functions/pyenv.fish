d1e7f721479fc0908d9fe19909034b2224388c28
Jeremiah England <englandtuning@gmail.com>
2022-07-25 22:19:20 -0400

    feat(pyenv): Test if in git root directory

--- private_dot_config/private_fish/functions/pyenv.fish
+++ private_dot_config/private_fish/functions/pyenv.fish
@@ -1,5 +1,11 @@
 function pyenv
     if test ! -d venv
+        if test ! -d .git
+            read answer -P 'You are not at a root directory of a git repository. Do you want to proceed? [Y/n] '
+            if string match -rq '^[^Yy]' "$answer"  # If $answer is empty then this does not match, so Y is the default.
+                return 0
+            end
+        end
         virtualenv venv
         source venv/bin/activate.fish
         if test -f requirements.txt
